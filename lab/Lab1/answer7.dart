9ec66f3951eb60b7744cb560fc942ffeb961ec77
Sushant <sstha813@gmail.com>
2023-04-08 20:56:24 +0545

    lab1 answer7 added

---
+++ lab/Lab1/answer7.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int i = 1; i <= 9; i++) {
+    for (int j = 1; j <= 10; j++) {
+      int result = i * j;
+      print("$i x $j = $result");
+    }
+    print(""); // Add a blank line between tables
+  }
+}
