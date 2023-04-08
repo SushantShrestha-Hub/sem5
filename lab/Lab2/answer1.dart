0457a701591a3e8f69ad2200604ba018eba7c101
Sushant <sstha813@gmail.com>
2023-04-08 21:03:42 +0545

    lab2 answer1 added

---
+++ lab/Lab2/answer1.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Create a list of names
+  List<String> names = ['adarsh', 'rachana', 'james', 'sid', 'manoj'];
+
+  // Print each name in the list
+  for (String name in names) {
+    print(name);
+  }
+}
